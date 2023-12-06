99fe334e58eb3d5becbccf3a31c62e238b5d3f46
LastnAccount <llorenz.10022002@gmail.com>
2023-12-06 11:05:16 +0800

    modify all

---
+++ projectsite/cardquest/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+
+# Register your models here.
+from .models import PokemonCard, Trainer, Collection
+
+#admin.site.register(PokemonCard)
+
+@admin.register (PokemonCard)
+class PokemonAdmin (admin. ModelAdmin):
+    list_display = ("name", "rarity") 
+    search_fields = ("name",)
