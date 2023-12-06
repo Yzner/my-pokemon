231f94f72407e0d345e32ac8891fce95f2178923
LastnAccount <llorenz.10022002@gmail.com>
2023-12-06 13:20:20 +0800

    modify all

--- projectsite/projectsite/urls.py
+++ projectsite/projectsite/urls.py
@@ -2,7 +2,7 @@
 URL configuration for projectsite project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -19,4 +19,22 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+]
+
+from django.urls import path
+from cardquest import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',views.HomePageView.as_view(),name='home'),
+]
+
+from cardquest.views import HomePageView, TrainerList, PokemonCardList ,CollectionList
+
+urlpatterns = [
+path('admin/', admin.site.urls), 
+path('', views.HomePageView.as_view(), name='home'),
+path('trainer_list', TrainerList.as_view(), name='trainer-list'), 
+path('pokemon-card-list', PokemonCardList.as_view(), name='pokemon-card-list'),
+path('collections/', CollectionList.as_view(), name='collection-list'),
 ]
